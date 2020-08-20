2e9ede5e1d22c1d1db134901651d56cc82526106
Ezra Ablaza <ezra.ablaza.inbox@gmail.com>
2020-08-20 03:58:18 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//app.js
+
+const Express = require("express");
+const { posix } = require("path");
+const Http = require("http").Server(Express);
+const Sio = require("socket.io")(Http);
+
+var pos = {
+    x: 200,
+    y: 200
+};
+
+var app = Express();
+
+
+Sio.on("connection", socket => {
+    Sio.emit("position", pos);
+});
+
+Http.listen(3000, () => {
+    console.log("listening at 3000")
+});
